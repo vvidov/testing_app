95d6041298e9570b1c9991856bd3ef9176efe16b
vvidov <vvidov@VVIDOV>
2024-09-03 14:18:23 +0200

    tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'models/favorites.dart';
+import 'screens/favorites.dart';
+import 'screens/home.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const TestingApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: HomePage.routeName,
+      builder: (context, state) {
+        return const HomePage();
+      },
+      routes: [
+        GoRoute(
+          path: FavoritesPage.routeName,
+          builder: (context, state) {
+            return const FavoritesPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+class TestingApp extends StatelessWidget {
+  const TestingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider<Favorites>(
+      create: (context) => Favorites(),
+      child: MaterialApp.router(
+        title: 'Testing Sample',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          ),
+          useMaterial3: true,
         ),
+        routerConfig: _router,
       ),
     );
   }
